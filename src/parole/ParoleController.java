917ccaadb27cf155940c1c5a957714e1c0e27978
rspotti <rspotti@INFO2-PC-CATT>
2018-09-19 11:16:42 +0200

    Versione Iniziale

---
+++ src/parole/ParoleController.java
@@ -0,0 +1,54 @@
+package parole;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import parole.model.Parole;
+
+public class ParoleController {
+	
+	Parole elenco ;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="txtParola"
+    private TextField txtParola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+    
+    @FXML
+    private Button btnReset;
+
+    @FXML // fx:id="btnInserisci"
+    private Button btnInserisci; // Value injected by FXMLLoader
+
+    @FXML
+    void doInsert(ActionEvent event) {
+    	// TODO
+    }
+    
+    @FXML
+    void doReset(ActionEvent event) {
+    	// TODO
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Parole.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Parole.fxml'.";
+        assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Parole.fxml'.";
+
+        elenco = new Parole() ;
+        
+    }
+}
